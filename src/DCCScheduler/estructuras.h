92ce8140abbea2591a40dc5604f1a81dad355794
Nicolas <ngulloa>
2025-04-11 19:01:39 -0400

    Primer Commit de Estructuras

--- src/DCCScheduler/estructuras.h
+++ src/DCCScheduler/estructuras.h
@@ -1,4 +1,25 @@
 #pragma once
 #include <stdio.h>	// FILE, fopen, fclose, etc.
 #include <stdlib.h> // malloc, calloc, free, etc
+#include <sys/types.h>
 #include "../file_manager/manager.h"
+
+typedef enum { HIGH, MEDIUM, LOW } QueueType;
+typedef enum { READY, WAITING, RUNNING, FINISHED} State;
+
+typedef struct proceso {
+    char nombre[256];
+    pid_t pid;
+    State estado;
+    time_t tiempo_rafagas;
+    int rafagas_ejecutando;
+    time_t tiempo_espera_io;
+    int prioridad;
+  } Proceso;
+
+typedef struct queue {
+    Proceso* primero;   // Primer proceso en la cola
+    Proceso* ultimo;    // Último proceso en la cola
+    QueueType tipo;
+    int quantum;
+} Queue;
