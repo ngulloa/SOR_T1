657d8baadf91d87db508f561c5efc889899dfc89
Nicolas <ngulloa>
2025-04-14 10:40:45 -0400

    estructuras listas - V1.0

--- src/DCCScheduler/estructuras.h
+++ src/DCCScheduler/estructuras.h
@@ -8,19 +8,38 @@ typedef enum { HIGH, MEDIUM, LOW } QueueType;
 typedef enum { READY, WAITING, RUNNING, FINISHED} State;
 
 typedef struct proceso {
+    // Variables de enunciado
     char* nombre;
     pid_t pid;
     State estado;
-    int tiempo_rafaga;
-    int n_rafagas;
-    int tiempo_espera_io;
+    int tiempo_rafaga;    // Tiempo de ejecución por ráfaga
+    int n_rafagas;        // Número de ejecución de ráfagas de ejecución en CPU
+    int tiempo_espera_io; // Tiempo de espera entre ráfagas
     int prioridad;
-    int tiempo_inicio
+    
+    // Variables propias
+    struct proceso* siguiente;
+    struct proceso* anterior;
+    int cambios_cola;
+    QueueType cola_asignada;
+
+    // Ticks
+    int tick_inicio; // Tick en el que el proceso entra por primera vez a la cola, útil para el calculo de response time
+    int tick_entrada; // Tick en el que entró a la CPU por primera vez. Útil para calcular response_time.
+    // Los tiempos se medirán en cantidad de tics y luego multiplicar por el periodo
+    int tick_finalizado; // Tick en el que el proceso finalizó, util para calcular turnaround time.
+    int ticks_waiting; // Ticks totales en el que el proceso está en estado WAITING y READY
+    int tiempo_response; // tiempo inicio - tiempo entrada
+
+    // Tiempos netos
+    double tiempo_rafaga_total;
+    double tiempo_espera_io_total;
   } Proceso;
 
 typedef struct queue {
     Proceso* primero;   // Primer proceso en la cola
     Proceso* ultimo;    // Último proceso en la cola
     QueueType tipo;
-    int quantum;
+    int quantum_base;
+    int quantum_actual;
 } Queue;
